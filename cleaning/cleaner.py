2fdd981f8b61437fb5ff14208c83cce2d89a440a
Mannvika <mannyjainlakes@gmail.com>
2025-08-14 13:07:59 -0400

    Updated everything to be modules and correctly import helper functions

--- cleaning/cleaner.py
+++ cleaning/cleaner.py
@@ -1,8 +1,27 @@
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 import pandas as pd
 import spacy
 import psycopg2
 from database.db_helper import connect_to_db, close_connection
 
+# Function to load database content into a pandas DataFrame
+def load_data_to_dataframe(connection):
+    query = "SELECT * FROM anime_data.anime;"
+    df = pd.read_sql_query(query, connection)
+    return df
 
-
+if __name__ == "__main__":
+    db_connection = connect_to_db()
+    if db_connection:
+        anime_df = load_data_to_dataframe(db_connection)
+        print("Data loaded into DataFrame successfully.")
+        print(anime_df.head())
+        close_connection(db_connection)
+    else:
+        print("Failed to connect to the database.")
 
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 import psycopg2
 import time
 from jikanpy import Jikan
