4a3a2408d7bbf4ce611a7d2df35479b0018967b0
Mannvika <mannyjainlakes@gmail.com>
2025-08-14 20:56:19 -0400

    Updated framework in prepartion to use firebase instead of manually handling authentication

--- app/models/user.py
+++ app/models/user.py
@@ -1,24 +1,12 @@
-from app.extensions import db, bcrypt
+from app.extensions import db
 
 class User(db.Model):
-
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
+    firebase_uid = db.Column(db.Text, unique=True, nullable=False)
 
     ratings = db.relationship('Rating', backref='user', lazy=True, cascade="all, delete-orphan")
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
-
-    def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
+    def __init__(self, firebase_id):
+        self.firebase_uid = firebase_id
